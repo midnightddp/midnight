de26f2bcdc0c527c1812d95477aeeefd5191875b
midnightddp <midnightddp@gmail.com>
2025-11-27 11:02:08 +0100

    changed the favicon of midnigt

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,67 +6,66 @@ import { Theme } from "@radix-ui/themes";
 
 // DM Mono
 const dmMono = DM_Mono({
-	subsets: ["latin"],
-	weight: ["400", "500"],
+  subsets: ["latin"],
+  weight: ["400", "500"],
 });
 
 // Outfit
 const outfit = Outfit({
-	subsets: ["latin"],
-	weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
-	title: "Midnight Drop",
-	description: "Be part of the  Drop, and the take part in the NIGHT token",
-	icons: [
-		{
-			rel: "icon",
-			url: "/faviconV2-16.png",
-			sizes: "16x16",
-			type: "image/png",
-		},
-		{
-			rel: "icon",
-			url: "/faviconV2-32.png",
-			sizes: "32x32",
-			type: "image/png",
-		},
-		{
-			rel: "icon",
-			url: "/faviconV2-48.png",
-			sizes: "48x48",
-			type: "image/png",
-		},
-		{
-			rel: "icon",
-			url: "/faviconV2-64.png",
-			sizes: "64x64",
-			type: "image/png",
-		},
-		{
-			rel: "icon",
-			url: "/faviconV2-128.png",
-			sizes: "128x128",
-			type: "image/png",
-		},
-		{
-			rel: "icon",
-			url: "/faviconV2-256.png",
-			sizes: "256x256",
-			type: "image/png",
-		},
-	],
+  title: "Midnight Drop",
+  description: "Be part of the  Drop, and the take part in the NIGHT token",
+  icons: [
+    {
+      rel: "icon",
+      url: "/favicon.ico",
+      sizes: "16x16",
+      type: "image/png",
+    },
+    {
+      rel: "icon",
+      url: "/favicon-16x16.png",
+      sizes: "32x32",
+      type: "image/png",
+    },
+    {
+      rel: "icon",
+      url: "/favicon-32x32.png",
+      sizes: "48x48",
+      type: "image/png",
+    },
+    {
+      rel: "icon",
+      url: "/apple-touch-icon.png",
+      type: "image/png",
+    },
+    {
+      rel: "icon",
+      url: "/android-chrome-192x192.png",
+      sizes: "192x192",
+      type: "image/png",
+    },
+    {
+      rel: "icon",
+      url: "/android-chrome-512x512.png",
+      sizes: "512x512",
+      type: "image/png",
+    },
+  ],
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{ children: React.ReactNode }>) {
-	return (
-		<html lang="en">
-			<body className="antialiased">
-				<Theme>{children}</Theme>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className="antialiased">
+        <Theme>{children}</Theme>
+      </body>
+    </html>
+  );
 }
